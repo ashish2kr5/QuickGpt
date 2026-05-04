8ad3eeee2028b60fc1d0a71a466624ef0b1e8f5c
Ashish Kumar <181575942+ashish2kr5@users.noreply.github.com>
2026-05-04 17:35:47 +0530

    third commit

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -107,6 +107,7 @@ export const stripeWebhooks = async (req, res) => {
         }
 
         const transaction = await Transaction.findById(transactionId);
+        
 
         if (!transaction || transaction.isPaid) {
           return res.json({ received: true });
